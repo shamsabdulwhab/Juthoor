30cfb3dd2b56b211f5662dc710ed0046576c7e56
shams <i494971@student.fontys.nl>
2025-06-10 13:33:23 +0200

    fixed the logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../components/icons/logo';
+import Logo from '../components/icons/logo.tsx';
 
 const Footer: React.FC = () => {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../components/icons/logo';
+import Logo from '../components/icons/logo.tsx';
 
 const Navbar: React.FC = () => {
   return (
